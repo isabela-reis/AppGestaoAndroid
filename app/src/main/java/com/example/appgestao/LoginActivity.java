2baa0c3a9f01ab6800155854406978eca26696b5
Isabela Reis <isabelareis.dev@gmail.com>
2024-05-05 17:26:05 -0300

    Configuração do Firebase e autenticação do Login

--- app/src/main/java/com/example/appgestao/LoginActivity.java
+++ app/src/main/java/com/example/appgestao/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.appgestao;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,34 +11,52 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.appgestao.model.Despesa;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
     private EditText TextUsername;
     private EditText TextPassword;
     private Button btnLogin;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         TextUsername = findViewById(R.id.TextUsername);
-       TextPassword = findViewById(R.id.TextPassword);
+        TextPassword = findViewById(R.id.TextPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = TextUsername.getText().toString().trim();
-                String password = TextPassword.getText().toString().trim();
-
-                if (true) {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show();
-                }
+                loginUser();
             }
         });
     }
+        private void loginUser() {
+            String email = TextUsername.getText().toString();
+            String password = TextPassword.getText().toString();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        }
 }
